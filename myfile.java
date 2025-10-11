4a5fe7df0aa0db7cc40d1bd00ccb09bfd604f12f
junaid63999 <junaidthodge665@gmail.com>
2025-10-11 05:07:22 +0000

    KKK

--- myfile.java
+++ myfile.java
@@ -4,6 +4,6 @@ public class myfile {
     public static void main(String[] args)
     {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("hotel bhagyashree");
+        System.out.println("hotel tiranga");
     }
 }
